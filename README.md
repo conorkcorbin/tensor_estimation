67252ec1e188ca3c16aa5417d8400f6c2bcefe93
Conor Corbin <ccorbin@usc.edu>
2018-02-26 14:43:10 -0800

    functional and cleaned up

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # tensor_estimation
 Estimate diffusion tensors from dMRI data. 
+
+## Dependencies
+Nibabel 2.2.1 (IO)
+Dipy 0.13.0 (IO)
+Numpy 1.13.3
+
+## Usage
+Given dwi volumes, a binary brainmask, bvecs, and bvals, will compute tensors at each voxel in mask and output FA, MD, RD, and AD maps. 
+
+python fit_tensors.py -dwi <filename.nii.gz> -mask <filename.nii.gz> -bvecs <filename.txt> -bvals <filename.txt> -out <prefix>
--- TensorEstimator.py
+++ TensorEstimator.py
@@ -1,106 +1,151 @@
 import numpy as np
+import nibabel as nib
+from dipy.io import read_bvals_bvecs
 
 class TensorEstimator:
 
-	def __init__(self):
-		""" Initialize tensor estimation object
-		
-		Parameters
-		----------
-		dwi : np array - shape = [N, X, Y ,Z]
-			diffusion signal from from each of N directions
-		mask : binary mask (gives indices where we will fit tensors)
-		bvecs : np array - shape = [N, 3]
-		bvals : np array - shape = [N x 1]
-
-		""" 
-		# self._dwi = dwi
-		# self._mask = mask
-		# self._bvecs = bvecs[1:]
-		# self._bvals = bvals
-		# self._bval = self._bvals[:-1] # assume last bval will be non-zero
-
-		# # for now just assume first volume is b0 and only single shell
-		# # index_bzero = np.argwhere(self._bvals==0)[0][0]
-		# # self._bzero = self._dwi[index_bzero]
-		# self._bzero = self._dwi[0, :, :, :]
-		# self._dwi = self._dwi[1:, :, :, :]
+    def __init__(self, f_dwi, f_mask, f_bvecs, f_bvals, f_out):
+        """ Initialize tensor estimation object
+        
+        Parameters
+        ----------
+        f_dwi: string 
+            file path to diffusion image
+        f_mask : string 
+            file path to mask
+        f_bvecs : string
+            file path to bvecs - unit vectors plz
+        f_bvals : string
+            file path to bvals
+        f_out : string
+            out prefix
 
 
-	def fit(self):
-		""" fit diffusion tensors to every voxel in mask 
+        """ 
+        dwi_img = nib.load(f_dwi)
+        mask_img = nib.load(f_mask)
+        dwi_data = dwi_img.get_data()
 
-		Uses least squares to fit tensor values from equation 
-		Y = WX 
-		Where Y = ln(So/Sk)/bval
-		W (the tensor vals we need to fit) : Dxx Dyy Dzz Dxy Dyz Dxz 
-			we know the tensor is a SPD symmetric matrix 
-		X : bvec coefficients
-		
-		# a 4D array of Y's 
-		"""
+        bvals, bvecs = read_bvals_bvecs(f_bvals, f_bvecs)
+        bzero_indices = np.argwhere(bvals==0).flatten()
+        dwi_indices = [i for i in range(len(bvals))
+                            if i not in bzero_indices]
 
-		# Y_vol = np.divide(np.log(np.divide(self._bzero, self._dwi)), self._bval) 
+        self.bzeros = np.delete(dwi_data, dwi_indices, axis=3)
+        self.dwi = np.delete(dwi_data, bzero_indices, axis=3)
+        self.bval = np.delete(bvals, bzero_indices)[0]
+        self.bvecs = np.delete(bvecs, bzero_indices, axis=0)
+        self.mask = mask_img.get_data()
+        shape = self.mask.shape
+        self.mask_indices = [(i, j, k) for k in range(shape[2])
+                                       for j in range(shape[1])
+                                       for i in range(shape[0])
+                                       if (self.mask[i, j, k] == 1 and 
+                                       not np.any(self.dwi[i, j, k, :] == 0))]  
+        self.affine = mask_img.get_affine()
+        self.f_out = f_out
 
+    def fit(self):
+        """ 
+        Fit diffusion tensors to every voxel in mask 
+        Least Squares Estimate
 
-	def _fit_voxel(self, Sk, So, bval, bvecs):
-		""" 
-		Fits a diffusion tensor at the voxel with dwi signals Sk and 
-		bzero signal So.
+        Calculate FA, MD, RD, AD from tensors and save to nifti files
 
-		Parameters
-		----------
-		Sk : list
-			of diffusion weighted intensity values at voxel
-		So : float
-			intensity at voxel of bzero
-		bval : float 
-			bval of the dwi
-		bvecs : np array shape = (N x 3)
-			unit vector describing direction of each dwi sig
+        """
+        tensors = [self._fit_voxel(self.dwi[i[0], i[1], i[2], :],
+                              self.bzeros[i[0], i[1], i[2], :],
+                              self.bval,
+                              self.bvecs)
+                              for i in self.mask_indices]
+        print("Tensors Estimated")
 
-		Returns
-		-------
-		tensor : np array shape = (3 x 3)
-			estimated diffusion tensor at this voxel
+        mean_diffusivity = [self._mean_diffusivity(t) for t in tensors]
+        print("MD calculated")
+        axial_diffusivity = [self._axial_diffusivity(t) for t in tensors]
+        print("AD calculated")
+        radial_diffusivity = [self._radial_diffusivity(t) for t in tensors]
+        print("RD calculated")
+        fractional_anisotropy = [self._fractional_anisotropy(t) for t in tensors]
+        print("FA calculated")
 
-		"""
-		# Y = np.divide(np.log(np.divide(self._bzero, dwi)), self._bval)
-		# X = [[g[0] ** 2, g[1] ** 2, g[2] ** 2, g[0]*g[1], g[1]*g[3], g[2]*g[3]]
-		# 		for g in self._bvecs]
-		# tensor = np.linalg.lstsq(X, Y)
+        self._save_nifti(mean_diffusivity, 'MD')
+        self._save_nifti(axial_diffusivity, 'AD')
+        self._save_nifti(radial_diffusivity, 'RD')
+        self._save_nifti(fractional_anisotropy, 'FA')
+        print("Saved Niftis")
 
-		Y = np.divide(np.log(np.divide(So, Sk)), bval) # shape = (N x 1)
-		print("log(so/sk) / b")
-		print(Y.shape)
-		print(Y)
-		X = np.array([np.array([g[0]**2, g[1]**2, g[2]**2, 2*g[0]*g[1], 2*g[0]*g[2], 2*g[1]*g[2]])
-				for g in bvecs]) # shape = (N x 6)
-		print("Coefficients from bvecs")
-		print(X.shape)
-		print(X)
-		W = np.linalg.lstsq(X, Y) # shape = (N x 6)
-		W = W[0]
-		print("least squares solution")
-		print(W.shape)
-		print(W)
-		tensor = np.array([[W[0], W[3], W[4]],
-						   [W[3], W[1], W[5]],
-						   [W[4], W[5], W[2]]])
-		print("Estimated Tensor")
-		print(tensor)
+    def _fit_voxel(self, Sk, So, bval, bvecs):
+        """ 
+        Fits a diffusion tensor at the voxel with dwi signals
+        Sk and bzero signal So.
 
+        Parameters
+        ----------
+        Sk : list
+            of diffusion weighted intensity values at voxel
+        So : list
+             of intensities at voxel for each bzero
+        bval : float 
+            bval of the dwi
+        bvecs : np array shape = (N x 3)
+            unit vector describing direction of each dwi sig
 
-		# Cramer's rule
-		W = np.linalg.solve(X, Y)
-		evals, evecs = np.linalg.eig(tensor)
-		print("Cramer's solution")
-		print(W)
-		print(evals)
-		print(evecs)
-		return tensor
+        Returns
+        -------
+        tensor : np array shape = (3 x 3)
+            estimated diffusion tensor at this voxel
 
+        """
+        So = np.average(So)
+        Y = np.divide(np.log(np.divide(So, Sk)), bval) # shape = (N x 1)
+        X = np.array([np.array([g[0]**2, g[1]**2,
+                                g[2]**2,2*g[0]*g[1],
+                                2*g[0]*g[2], 2*g[1]*g[2]])
+                                for g in bvecs]) # shape = (N x 6)
+        W = np.linalg.lstsq(X, Y) # shape = (N x 6)
+        W = W[0] # just solutions (not residuals)
+        tensor = np.array([[W[0], W[3], W[4]],
+                           [W[3], W[1], W[5]],
+                           [W[4], W[5], W[2]]])
+        return tensor
 
+    def _mean_diffusivity(self, tensor):
+        """ Returns mean diffusivity from tensor """
+
+        evals, evecs = np.linalg.eig(tensor)
+        # self.evals.append(np.sort(evals))
+        return np.average(evals)
+
+    def _fractional_anisotropy(self, tensor):
+        evals, evecs = np.linalg.eig(tensor)
+
+        mean_eval = np.average(evals)
+        numerator = np.sqrt((evals[0]-mean_eval)**2 +
+                            (evals[1]-mean_eval)**2 +
+                            (evals[2]-mean_eval)**2)
+        denominator = np.sqrt(evals[0]**2 +
+                              evals[1]**2 +
+                              evals[2]**2)
+        return np.sqrt(3.0/2.0) * (numerator/denominator)
+
+    def _axial_diffusivity(self, tensor):
+        evals, evecs = np.linalg.eig(tensor)
+        return np.max(evals)
+
+    def _radial_diffusivity(self, tensor):
+        evals, evecs = np.linalg.eig(tensor)
+        evals = np.sort(evals)
+        return (evals[0] + evals[1])/2
+
+    def _save_nifti(self, measure, suffix):
+        """ Saves a nifti of the computed measure """ 
+        shape = self.mask.shape
+        image_data = np.zeros(shape)
+        for i, ind in enumerate(self.mask_indices):
+            image_data[ind[0], ind[1], ind[2]] = measure[i]
+        image = nib.Nifti1Image(image_data, self.affine)
+        nib.save(image, ''.join([self.f_out, '_', suffix, '.nii.gz']))
 
 
 
